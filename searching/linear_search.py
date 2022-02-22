af9a7160fcb0385ab155b03e0b3ed1df6e164948
dynstat <vivek15@gmx.com>
2022-02-23 01:27:11 +0530

    recursive linear search function added

--- searching/linear_search.py
+++ searching/linear_search.py
@@ -8,10 +8,21 @@ def linear_search(array: list, to_find: int):
     return -1
 
 
+def recursive_linear_search(array, start_index, end_index, to_find):
+    if array[start_index] == to_find:
+        return start_index
+    if array[end_index] == to_find:
+        return end_index
+    if start_index > end_index:
+        return -1
+    recursive_linear_search(array, start_index+1, end_index-1, to_find)
+
+
 if __name__ == "__main__":
     arr = [4, 6, 7, 8, 2, 9, 3, 0, ]
-    to_find = int(input(f"Enter the number to find in the given array\n"))
-    output = linear_search(arr, to_find)
+    to_find = int(input('Enter the number to find in the given array\n'))
+    # output = linear_search(arr, to_find)
+    output = recursive_linear_search(arr, 0, 5, to_find)
 
     if output != -1:
         print(f"The item {to_find} was found at index = {output}\n")
