1a3b150772abb19eb6165bbb27b853efc0225dc8
dynstat <vivek15@gmx.com>
2022-02-24 02:06:10 +0530

    double linear search using recursion is added

--- searching/linear_search.py
+++ searching/linear_search.py
@@ -1,6 +1,6 @@
 # simple and recursive linear search in python
 
-from tracemalloc import start
+import sys
 
 
 def linear_search(array: list[int], to_find: int):
@@ -18,7 +18,8 @@ def recursive_linear_search(array, start_index, end_index, to_find):
         return end_index
     if start_index > end_index:
         return -1
-    recursive_linear_search(array, start_index+1, end_index-1, to_find)
+    # why not calling recursive function without return below ??
+    return recursive_linear_search(array, start_index+1, end_index-1, to_find)
 
 
 def double_linear_search(array: list[int], start_index, end_index, to_find):
@@ -33,12 +34,27 @@ def double_linear_search(array: list[int], start_index, end_index, to_find):
     return -1
 
 
+def recursive_double_linear_search(array: list[int], start_index, end_index, to_find):
+    if array[start_index] == to_find:
+        return start_index
+    if array[end_index] == to_find:
+        return end_index
+    if start_index > end_index:
+        return -1
+    return recursive_double_linear_search(array, start_index+1, end_index-1, to_find)
+
+
 if __name__ == "__main__":
     arr = [4, 6, 7, 8, 2, 9, 3, 0, ]
-    to_find = int(input('Enter the number to find in the given array\n'))
+    try:
+        to_find = int(input('Enter the number to find in the given array\n'))
+    except:
+        print("invalid input")
+        sys.exit(0)
     # output = linear_search(arr, to_find)
     # output = double_linear_search(arr, 0, 5, to_find)
-    output = recursive_linear_search(arr, 0, 5, to_find)
+    # output = recursive_linear_search(arr, 0, 5, to_find)
+    output = recursive_double_linear_search(arr, 0, 5, to_find)
 
     if output != -1:
         print(f"The item {to_find} was found at index = {output}\n")
