538d699d676305deac0d8948c35670937b68d002
dynstat <vivek15@gmx.com>
2022-02-23 19:04:47 +0530

    added double linear search function

--- searching/linear_search.py
+++ searching/linear_search.py
@@ -1,6 +1,9 @@
 # simple and recursive linear search in python
 
-def linear_search(array: list, to_find: int):
+from tracemalloc import start
+
+
+def linear_search(array: list[int], to_find: int):
     # sourcery skip: use-next,--> return next((index for (index, value) in enumerate(array) if value == to_find), -1)
     for (index, value) in enumerate(array):
         if value == to_find:
@@ -18,10 +21,23 @@ def recursive_linear_search(array, start_index, end_index, to_find):
     recursive_linear_search(array, start_index+1, end_index-1, to_find)
 
 
+def double_linear_search(array: list[int], start_index, end_index, to_find):
+    while start_index <= end_index:
+        if array[start_index] == to_find:
+            return start_index
+        elif array[end_index] == to_find:
+            return end_index
+        else:
+            start_index += 1
+            end_index += 1
+    return -1
+
+
 if __name__ == "__main__":
     arr = [4, 6, 7, 8, 2, 9, 3, 0, ]
     to_find = int(input('Enter the number to find in the given array\n'))
     # output = linear_search(arr, to_find)
+    # output = double_linear_search(arr, 0, 5, to_find)
     output = recursive_linear_search(arr, 0, 5, to_find)
 
     if output != -1:
